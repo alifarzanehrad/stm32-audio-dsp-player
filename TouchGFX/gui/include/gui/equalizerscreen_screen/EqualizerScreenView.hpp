d68223ed5b6e6c795ef3062fac15534f2a252fc4
alifarzanehrad <alifarzane.rad@gmail.com>
2026-08-17 20:30:00 +0330

    feat: implement equalizer gain controls

--- TouchGFX/gui/include/gui/equalizerscreen_screen/EqualizerScreenView.hpp
+++ TouchGFX/gui/include/gui/equalizerscreen_screen/EqualizerScreenView.hpp
@@ -9,9 +9,25 @@ class EqualizerScreenView : public EqualizerScreenViewBase
 public:
     EqualizerScreenView();
     virtual ~EqualizerScreenView() {}
+
     virtual void setupScreen();
     virtual void tearDownScreen();
+
+    void slider100HzChanged(int value) override;
+    void slider300HzChanged(int value) override;
+    void slider1kHzChanged(int value) override;
+    void slider3kHzChanged(int value) override;
+    void slider8kHzChanged(int value) override;
+
+    void flatButtonClicked() override;
+
 protected:
+    void updateGainText(
+        touchgfx::TextAreaWithOneWildcard& textArea,
+        touchgfx::Unicode::UnicodeChar* buffer,
+        uint16_t bufferSize,
+        int sliderValue
+    );
 };
 
 #endif // EQUALIZERSCREENVIEW_HPP

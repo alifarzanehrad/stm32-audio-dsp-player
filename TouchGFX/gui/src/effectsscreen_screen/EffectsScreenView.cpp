759cb4782875904fedc690470b8b08a06c9e94e4
alifarzanehrad <alifarzane.rad@gmail.com>
2026-08-20 19:29:04 +0330

    feat(ui): connect echo toggle to presenter

--- TouchGFX/gui/src/effectsscreen_screen/EffectsScreenView.cpp
+++ TouchGFX/gui/src/effectsscreen_screen/EffectsScreenView.cpp
@@ -8,9 +8,15 @@ EffectsScreenView::EffectsScreenView()
 void EffectsScreenView::setupScreen()
 {
     EffectsScreenViewBase::setupScreen();
+    Echo_toggle.forceState(presenter->getEchoEnabled());
 }
 
 void EffectsScreenView::tearDownScreen()
 {
     EffectsScreenViewBase::tearDownScreen();
+}
+
+void EffectsScreenView::echoToggled()
+{
+    presenter->setEchoEnabled(Echo_toggle.getState());
 }

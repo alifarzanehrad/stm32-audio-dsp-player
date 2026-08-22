1c3cbfae928b2a19653f7e1ba2fdf841ff9c4dc2
alifarzanehrad <alifarzane.rad@gmail.com>
2026-08-22 22:23:23 +0330

    refactor(usb): remove unused OTG FS IRQ declaration

--- Core/Inc/stm32f7xx_it.h
+++ Core/Inc/stm32f7xx_it.h
@@ -55,7 +55,7 @@ void DebugMon_Handler(void);
 void SDMMC1_IRQHandler(void);
 void TIM6_DAC_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
-void OTG_FS_IRQHandler(void);
+
 void DMA2_Stream6_IRQHandler(void);
 void LTDC_IRQHandler(void);
 void DMA2D_IRQHandler(void);
